910f9c841a566d10f32c00e4354432cbb0b2f841
Deploy <deploy@api-baitafeira>
2020-06-19 18:41:55 +0000

    Adicionando a mensagem de retorno ao digitar "Olá!"

--- bot.js
+++ bot.js
@@ -8,12 +8,19 @@ const WelcomeService = require('./Services/Welcome');
 
 client.on('ready', () => {
     client.user.setActivity(`Let's Code`);
+    console.log('O bot está on-line');
 });
 
 client.on('guildMemberAdd', async member => {
     const welcomeService = new WelcomeService(member);
 
     await welcomeService.generateImage();
+});
+
+client.on('message', msg => {
+	if(msg.content === "Olá!"){
+		msg.reply('Olá meu amigo');
+	}
 });
 
 client.login(process.env.AUTH_TOKEN);
